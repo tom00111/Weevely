e4c740f60882ec681485e46ac2fb2747ba610d2a
emilio.pinn@gmail.com <emilio.pinn@gmail.com@5205769a-2ae6-11df-b744-258d51847b74>
2011-10-11 15:50:09 +0000

    File.upload check previous file existance
    git-svn-id: http://weevely.googlecode.com/svn/trunk@136 5205769a-2ae6-11df-b744-258d51847b74

--- modules/file/upload.py
+++ modules/file/upload.py
@@ -33,6 +33,11 @@ class Upload(Module):
     def __execute_payload(self, interpreter, vector, file_encoded_content, remote_path, file_local_md5):
         
         
+        
+        file_exists = self.modhandler.load('file.check').run(remote_path, 'exists' , True)
+        if file_exists:
+            raise ModuleException(self.name,  'File \'%s\' exists, change remote path' % remote_path)
+            
         output = ''
         
         payload = self.vectors[interpreter][vector] % (remote_path, self.rand_post_name)
