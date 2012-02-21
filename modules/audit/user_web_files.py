bd673ef3fbdb936487a65886f1655a1b9c732924
emilio.pinn@gmail.com <emilio.pinn@gmail.com@5205769a-2ae6-11df-b744-258d51847b74>
2012-02-21 17:53:07 +0000

    audit.user_web_files clean
    git-svn-id: http://weevely.googlecode.com/svn/trunk@241 5205769a-2ae6-11df-b744-258d51847b74

--- modules/audit/user_web_files.py
+++ modules/audit/user_web_files.py
@@ -1,16 +1,20 @@
 
 
 from core.module import Module, ModuleException
+from core.parameters import ParametersList, Parameter as P
 from external.crawler import Crawler
 
 classname = 'UserWebFiles'
 
 class UserWebFiles(Module):
     """Enumerate w/r/x files in web folders
-    :audit.user_web_files /home/user/public_html/ http://www.site.com/user/
+    :audit.user_web_files http://www.site.com/user/ /home/user/public_html/ 
     """
-    
-    
+    params = ParametersList('First crawl web site, then enumerate files searching w/r/x permissions', None,
+            P(arg='url', help='Site to crawl', required=True, pos=0),
+            P(arg='rpath', help='Remote root path corresponding to crawled site', required=True, pos=1),
+            P(arg='deep', help='Crawl deepness', default=3, type=int, pos=2),
+            )
 
     def __init__( self, modhandler , url, password):
 
@@ -19,9 +23,8 @@ class UserWebFiles(Module):
         self.usersfiles = {}    
         
         
-    def run(self, home, url, depth_limit = 3):
+    def run_module(self, url, home, depth_limit):
         
-        depth_limit = 5
         confine_prefix = url
         exclude = ''
 
@@ -40,7 +43,7 @@ class UserWebFiles(Module):
         self.mprint('[%s] Enumerating %i paths in %s' % (self.name, len(path_list), home))
 
         if path_list:
-            self.modhandler.load('enum.paths').run('', path_list)
+            self.modhandler.load('enum.paths').run_module('', path_list)
                     
                        
                             
